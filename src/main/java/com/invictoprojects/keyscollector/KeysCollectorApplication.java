8482717d2e6a4088c2982a5420e54aa203306fc8
Koroliuk <koroliuk.yaroslav@gmail.com>
2022-06-09 20:36:39 +0300

    Create project

---
+++ src/main/java/com/invictoprojects/keyscollector/KeysCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.invictoprojects.keyscollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeysCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeysCollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invictoprojects/keyscollector/KeysCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invictoprojects.keyscollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeysCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
