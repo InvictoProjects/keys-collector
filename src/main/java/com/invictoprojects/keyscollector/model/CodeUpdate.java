a6e81c1a68f4d70f1c9a11b77cba15e7fa0533e0
Pienskoi <vladymyrpenskoy@gmail.com>
2022-06-26 15:16:14 +0300

    Unpack nested JSON properties

--- src/main/java/com/invictoprojects/keyscollector/model/CodeUpdate.java
+++ src/main/java/com/invictoprojects/keyscollector/model/CodeUpdate.java
@@ -1,10 +1,16 @@
 package com.invictoprojects.keyscollector.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class CodeUpdate {
 
@@ -17,10 +23,20 @@ public class CodeUpdate {
     @JsonProperty("url")
     private String url;
 
+    private String repositoryName;
+
     @JsonProperty("repository")
-    private Repository repository;
+    private void unpackRepositoryName(Map<String, Object> repository) {
+        this.repositoryName = (String)repository.get("name");
+    }
+
+    private List<String> textMatches;
 
     @JsonProperty("text_matches")
-    private List<TextMatches> textMatches;
+    private void unpackFragments(List<Map<String, Object>> textMatches) {
+        this.textMatches = textMatches.stream()
+                .map(textMatch -> (String)textMatch.get("fragment"))
+                .collect(Collectors.toList());
+    }
 
 }
--- src/main/java/com/invictoprojects/keyscollector/model/CodeUpdates.java
+++ src/main/java/com/invictoprojects/keyscollector/model/CodeUpdates.java
@@ -1,10 +1,14 @@
 package com.invictoprojects.keyscollector.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class CodeUpdates {
 
